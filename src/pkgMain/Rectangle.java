caf3cfc727eede8b3d3f2b24dcf7177138dd913d
Hunter <Hunter@128.4.91.40>
2017-02-08 17:35:53 -0500

    implemented area method

--- src/pkgMain/Rectangle.java
+++ src/pkgMain/Rectangle.java
@@ -4,20 +4,26 @@ public class Rectangle {
 
 	private double Length;
 	private double Width;
-	public Rectangle(double length, double width) {
+	public Rectangle(double length, double width) 
+	{
 		Length = length;
 		Width = width;
 	}
-	public double getLength() {
+	public double getLength() 
+	{
 		return Length;
 	}
-	public double getWidth() {
+	public double getWidth() 
+	{
 		return Width;
 	}
 	
 	public double Area()
 	{
-		// TODO Implement Area() function with the correct formula
-		return 0;
+		// TODO Implement wArea() function with the correct formula
+		double x = getLength() * getWidth();
+		return x;
 	}	
+	
 }
+
